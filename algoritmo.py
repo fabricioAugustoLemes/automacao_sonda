750f6db0c83efe674ad823c53189cd73669fdd50
Fabrício <fabricio.gold385@gmail.com>
2025-04-18 18:42:05 -0300

    tes1

---
+++ algoritmo.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+
+
+# Aguarde um pouco antes de executar o comando (opcional)
+print("O programa começará em 3 segundos. Alterne para a janela onde deseja executar.")
+time.sleep(3)
+
+pyautogui.click(x=100, y=200)
+
